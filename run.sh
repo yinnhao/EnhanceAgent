bdfe6e21140e07a959102b921d21bed2a436813e
yinnhao <1048319334@qq.com>
2025-10-11 17:35:11 +0800

    run denoise and sr successfully

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-# python cli_main.py --image ./test.png --instruction "先灰度化再顺时针旋转90度" --output ./output/test_out.png
-python cli_main.py --image ./KAIR/testsets/real3/palace.png --instruction "去噪" --output ./output/palace_test_out.png
+# python cli_main.py --image ./KAIR/testsets/real3/palace.png --instruction "先灰度化再顺时针旋转90度" --output ./output/test_out.png
+# python cli_main.py --image ./KAIR/testsets/real3/palace.png --instruction "去噪" --output ./output/palace_test_out.png
+python cli_main.py --image ./KAIR/testsets/real3/palace.png --instruction "先去噪再超分" --output ./output/palace_test_out2.png
+python cli_main.py --image ./KAIR/testsets/real3/palace.png --instruction "噪声少一点" --output ./output/palace_test_out3.png
