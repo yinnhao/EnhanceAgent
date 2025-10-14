f30e5be9b3c5a33e79a5c5f10379bdc475ef06e4
yinnhao <1048319334@qq.com>
2025-10-14 16:36:28 +0800

    run colorize successful

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 # python cli_main.py --image ./KAIR/testsets/real3/palace.png --instruction "先灰度化再顺时针旋转90度" --output ./output/test_out.png
 # python cli_main.py --image ./KAIR/testsets/real3/palace.png --instruction "去噪" --output ./output/palace_test_out.png
-python cli_main.py --image ./KAIR/testsets/real3/palace.png --instruction "先去噪再超分" --output ./output/palace_test_out2.png
-python cli_main.py --image ./KAIR/testsets/real3/palace.png --instruction "噪声少一点" --output ./output/palace_test_out3.png
+# python cli_main.py --image ./KAIR/testsets/real3/palace.png --instruction "先去噪再超分" --output ./output/palace_test_out2.png
+# python cli_main.py --image ./KAIR/testsets/real3/palace.png --instruction "噪声少一点" --output ./output/palace_test_out3.png
+python cli_main.py --image DDColor/assets/test_images/test.jpg --instruction "先上色再超分" --output ./output/colorize_test_out3.png
+
